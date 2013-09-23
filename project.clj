e249ca15a2e055e6503ce768615822db591d5a90
magomimmo <mimmo.cosenza@gmail.com>
2013-09-23 23:23:49 +0200

    updated to 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,14 +1,10 @@
-(defproject shoreleave/shoreleave-remote-ring "0.3.0"
+(defproject shoreleave/shoreleave-remote-ring "0.3.1-SNAPSHOT"
   :description "A smarter client-side with ClojureScript : Ring- (and Compojure-) server-side Remotes support"
   :url "https://github.com/shoreleave/shoreleave-remote-ring"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "See the notice in README.mkd or details in LICENSE_epl.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.reader "0.7.0"]]
-  :dev-dependencies [;[cdt "1.2.6.2-SNAPSHOT"]
-                     ;[lein-cdt "1.0.0"] ; use lein cdt to attach
-                     ;[lein-autodoc "0.9.0"]
-                     [lein-marginalia "0.7.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.reader "0.7.7"]])
 
