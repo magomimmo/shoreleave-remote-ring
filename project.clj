7a94d72bf9ba07e44bfcbee66e4feca2eeaaa9d3
magomimmo <mimmo.cosenza@gmail.com>
2013-11-23 09:05:54 +0100

    prepare for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.magomimmo/shoreleave-remote-ring "0.3.1-SNAPSHOT"
+(defproject org.clojars.magomimmo/shoreleave-remote-ring "0.3.1"
   :description "A smarter client-side with ClojureScript : Ring- (and Compojure-) server-side Remotes support"
-  :url "https://github.com/magomimmo/shoreleave-remote-ring/tree/clojars"
+  :url "https://github.com/magomimmo/shoreleave-remote-ring/tree/0.3.1"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
